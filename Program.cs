057f53fc189b79d5cdbfc7726d7bc54c88001939
Marcelo Heredia <marcelo.heredia@acad.pucrs.br>
2021-04-06 20:40:37 -0300

    initial GeneticSharp usage (not compiling yet)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using GeneticSharp.Domain;
+using GeneticSharp.Domain.Crossovers;
+using GeneticSharp.Domain.Fitnesses;
+using GeneticSharp.Domain.Mutations;
+using GeneticSharp.Domain.Selections;
+using GeneticSharp.Infrastructure.Framework.Threading;
 
 namespace Integradora2
 {
@@ -6,7 +12,21 @@ namespace Integradora2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var taskExecutor = new ParallelTaskExecutor();
+            taskExecutor.MinThreads = 4;
+            taskExecutor.MaxThreads = 11;
+
+
+            var crossover = new TwoPointCrossover();
+            var selection = new TournamentSelection(5);
+            var mutation = new FlipBitMutation();
+            var ga = new GeneticAlgorithm();
+
+            ga.MutationProbability = 0.2f;
+            ga.CrossoverProbability = 0.65f;
+
+
+            ga.TaskExecutor = taskExecutor;
         }
     }
 }
