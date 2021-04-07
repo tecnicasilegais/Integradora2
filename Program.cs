63261e3d5a66bc051862a0cfdcb79bfafe7dfddd
Eduardo C. Andrade <eduardo.a@edu.pucrs.br>
2021-04-07 19:33:13 -0300

    Added connection to MySQL

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using GeneticSharp.Domain.Fitnesses;
 using GeneticSharp.Domain.Mutations;
 using GeneticSharp.Domain.Selections;
 using GeneticSharp.Infrastructure.Framework.Threading;
+using MySql.Data.MySqlClient;
 
 namespace Integradora2
 {
@@ -12,6 +13,9 @@ namespace Integradora2
     {
         static void Main(string[] args)
         {
+            ConnectDb();
+
+            /*
             var taskExecutor = new ParallelTaskExecutor();
             taskExecutor.MinThreads = 4;
             taskExecutor.MaxThreads = 11;
@@ -25,8 +29,22 @@ namespace Integradora2
             ga.MutationProbability = 0.2f;
             ga.CrossoverProbability = 0.65f;
 
-
             ga.TaskExecutor = taskExecutor;
+            */
+        }
+
+        static void ConnectDb()
+        {
+            var user = Environment.GetEnvironmentVariable("integradora_user", EnvironmentVariableTarget.User);
+            var password = Environment.GetEnvironmentVariable("integradora_password", EnvironmentVariableTarget.User);
+            var connectionStr = $"Server=localhost;Uid={user};Pwd={password};Database=tpch;";
+            using var connection = new MySqlConnection(connectionStr);
+            connection.Open();
+            var query = "SELECT C_MKTSEGMENT FROM tpch.customer where C_CUSTKEY=10;";
+            var queryCmd = new MySqlCommand(query, connection);
+
+            var result = queryCmd.ExecuteScalar().ToString();
+            Console.WriteLine(result);
         }
     }
 }
