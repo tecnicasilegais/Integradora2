52a7be0e75e835a434b0bf7a7ce70637cc9117f5
Eduardo C. Andrade <eduardo.a@edu.pucrs.br>
2021-04-21 21:52:00 -0300

    Added queries

--- db_connection.py
+++ db_connection.py
@@ -1,17 +1,16 @@
 import mysql.connector
 import os
 
+import queries
+
 USER = os.environ['integradora_user']
 PASSWORD = os.environ['integradora_password']
 
 connection = mysql.connector.connect(user=USER, password=PASSWORD, host='localhost', database='tpch')
 cursor = connection.cursor()
 
-query = ("SELECT C_MKTSEGMENT FROM tpch.customer where C_CUSTKEY=10;")
-cursor.execute(query)
-
-for (c_mktsegments) in cursor:
-    print(c_mktsegments)
+for query in enumerate(queries.q):
+    cursor.execute(query)
 
 cursor.close()
 connection.close()
