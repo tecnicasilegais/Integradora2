b5bf196ed8a8dd9cb8e3fa00b40d09951647b45e
Marcelo Heredia <marcelo.heredia@acad.pucrs.br>
2021-04-21 22:17:51 -0300

    db class

--- db_connection.py
+++ db_connection.py
@@ -7,21 +7,22 @@ import queries
 USER = os.environ['integradora_user']
 PASSWORD = os.environ['integradora_password']
 
-connection = mysql.connector.connect(user=USER, password=PASSWORD, host='localhost', database='tpch')
-cursor = connection.cursor()
 
+class Db:
+    def __init__(self, name):
+        self.conn = mysql.connector.connect(user=USER, password=PASSWORD, host='localhost', database='tpch')
+        self.cursor = self.conn.cursor()
 
-def execute_queries():
-    start = time.time()
-    for query in enumerate(queries.q):
-        cursor.execute(query)
-    end = time.time()
-    return end - start
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
 
+    def execute_queries(self):
+        start = time.time()
+        for query in enumerate(queries.q):
+            self.cursor.execute(query)
+        end = time.time()
+        return end - start
 
-def simulate_individual(individual):
-    execute_queries()
-
-
-cursor.close()
-connection.close()
+    def simulate_individual(self, individual):
+        return self.execute_queries()
