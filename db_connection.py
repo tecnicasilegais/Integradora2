c7e00651c082ee2420dbd21986892734324e82e9
Marcelo Heredia <marcelo.heredia@acad.pucrs.br>
2021-04-21 22:42:30 -0300

    index size and table sizes

--- db_connection.py
+++ db_connection.py
@@ -11,15 +11,25 @@ PASSWORD = os.environ['integradora_password']
 class Db:
     def __init__(self, name):
         self.conn = mysql.connector.connect(user=USER, password=PASSWORD, host='localhost', database='tpch')
-        self.cursor = self.conn.cursor()
+        self.cursor = self.conn.cursor
+        self.initial_index_size = self.get_index_size()
+        self.biggest_tables = self.get_biggest_tables()
 
     def close(self):
         self.cursor.close()
         self.conn.close()
 
+    def get_biggest_tables(self):
+        self.cursor.execute('todo')
+        return self.cursor.biggest
+
+    def get_index_size(self):
+        self.cursor.execute(queries.index_size)
+        return self.cursor.index_size
+
     def execute_queries(self):
         start = time.time()
-        for query in enumerate(queries.q):
+        for query in queries.select:
             self.cursor.execute(query)
         end = time.time()
         return end - start
