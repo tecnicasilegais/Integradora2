c7f7147a32c06f90983d5e3bfbc6e05c71860106
Marcelo Heredia <marcelo.heredia@acad.pucrs.br>
2021-04-23 17:00:38 -0300

    gadis reprod should be ok now

--- gadis_reprod.py
+++ gadis_reprod.py
@@ -1,6 +1,8 @@
 import random
+import logging
 import numpy as np
 from deap import creator, base, tools, algorithms
+import util
 
 # instances
 import db_connection
@@ -16,6 +18,8 @@ NGEN = 100  # number of generations
 K_ELITE = 1
 K_TOURNAMENT = 5
 db = db_connection.Db()
+
+logging.basicConfig(filename=util.make_filename('gen_execution.log'), encoding='utf-8', level=logging.DEBUG)
 
 creator.create("FitnessMulti", base.Fitness, weights=(-1.0, -0.5))  # define weights
 creator.create("Individual", list, typecode='b', fitness=creator.FitnessMulti)
@@ -35,45 +39,79 @@ toolbox.register('select', tools.selNSGA)
 def fitness_func(individual):
     return db.simulate_individual(individual)
 
+
 toolbox.register('evaluate', fitness_func)
 
 
+def stop_criteria(best_fits):
+    return np.all(best_fits == best_fits[0])
+
+
 def main():
+    best_fits = []
     random.seed(64)
     pop = toolbox.population(n=POP_SIZE)
 
-    fitnesses = list(map(toolbox.evaluate, pop))
-    for ind, fit in zip(pop, fitnesses):
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    '''for ind, fit in zip(pop, fitnesses):
         ind.fitness.values = fit
     print("Evaluated %i individuals" % len(pop))
     fits = [ind.fitness.values[0] for ind in pop]
-    g = 0
+    g = 0'''
 
-    while min(fits) > 0 and g < NGEN:
-        g += 1
-        offspring = toolbox.select(pop)
-        offspring = list(map(toolbox.clone, offspring))
+    pop = toolbox.select(pop, len(pop))
+
+    # Begin the generational process
+    for gen in range(1, NGEN):
+
+        # Vary the population
+        offspring = tools.selTournamentDCD(pop, K_TOURNAMENT)
+        offspring = [toolbox.clone(ind) for ind in offspring]
+
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
-            if rng.random() < CXPB:
+            if rng.random() <= CXPB:
                 toolbox.mate(child1, child2)
                 del child1.fitness.values
                 del child1.fitness.values
+
         for mutant in offspring:
             if rng.random() < MUTPB:
                 toolbox.mutate(mutant)
                 del mutant.fitness.values
+
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
         fitnesses = map(toolbox.evaluate, invalid_ind)
+
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
-        pop[:] = offspring
+
+        pop = toolbox.select(pop + offspring, POP_SIZE)
         fits = [ind.fitness.values[0] for ind in pop]
-        print('generation %i:' % g)
-        print("fitness --", min(fits))
+
+        print('generation %i:' % gen)
+        bestf = min(fits)
+        print("fitness --", bestf)
+
+        best_ind = tools.selBest(pop, 1)[0]
+        logging.info('Generation: %i, best individual: %s, fitness: %s' % gen, best_ind, bestf)
+        best_fits.append(bestf)
+
+        if len(best_fits) > 10:
+            best_fits = best_fits[-10:]
+            if stop_criteria(best_fits):
+                logging.info('Stop criteria reached')
+                break
 
     best_ind = tools.selBest(pop, 1)[0]
     print(best_ind)
-    print("fitness --", min(fits))
+    print(min(fits))
+    logging.info('END RESULT: best individual: %s, fitness: %s' % best_ind, min(fits))
 
 
 if __name__ == '__main__':
---
+++ util.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+
+def make_filename(name):
+    return "%s%s" % datetime.now().strftime("%d_%H-%M-%S_"), name
