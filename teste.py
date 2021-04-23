55c712b70f9ea9a8c15af4714a572fcd1374deb3
Eduardo C. Andrade <eduardo.a@edu.pucrs.br>
2021-04-23 16:48:08 -0300

    added time counter

--- teste.py
+++ teste.py
@@ -1,3 +1,5 @@
+import time
+
 import db_connection
 import numpy as np
 
@@ -5,9 +7,12 @@ db = db_connection.Db()
 
 # db.debug()
 
+start = time.time()
 for i in range(0, 5000):
     individual = np.random.randint(2, size=22)
     db.simulate_individual(individual)
     print(i)
 
+end = time.time()
+print(end - start)
 db.close()
